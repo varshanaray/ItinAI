19d03b286affa27efa14ddea821c6fdafc30ef54
Serylda <ericyzt1an@gmail.com>
2024-03-16 17:12:15 -0500

    added group page VC

---
+++ itinAI-Project/GroupPageVC.swift
@@ -0,0 +1,43 @@
+//
+//  HomeViewController.swift
+//  itinAI-Project
+//
+//  Created by Eric Yang on 3/7/24.
+//
+
+import UIKit
+
+class GroupPageVC: UIViewController {
+
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var contentView: UIView!
+    
+    @IBOutlet weak var peopleTableView: UITableView!
+    
+    @IBOutlet weak var citiesTableView: UITableView!
+    @IBOutlet weak var announcementsTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        contentView.layer.cornerRadius = 20
+        
+        citiesTableView.layer.cornerRadius = 20
+        citiesTableView.layer.borderWidth = 1
+        citiesTableView.layer.borderColor = UIColor.darkGray.cgColor
+        
+        peopleTableView.layer.cornerRadius = 20
+        peopleTableView.layer.borderWidth = 1
+        peopleTableView.layer.borderColor = UIColor.darkGray.cgColor
+        
+        announcementsTableView.layer.cornerRadius = 20
+        announcementsTableView.layer.borderWidth = 1
+        announcementsTableView.layer.borderColor = UIColor.darkGray.cgColor
+        
+    }
+    
+    
+
+}
