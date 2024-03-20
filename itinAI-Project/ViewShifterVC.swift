628d20b3db7b19edaa41b4786de1eb356701e288
Erick Albarran <erickalbarran08@gmail.com>
2024-03-19 19:26:19 -0500

    Blank keyboard shifter VC

---
+++ itinAI-Project/ViewShifterVC.swift
@@ -0,0 +1,26 @@
+// Project: itinAI-Alpha
+// EID: ezy78, gkk298, esa549, vn4597
+// Course: CS371L
+
+import UIKit
+
+class ViewShifterVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
