4abb9a5b659e69c2962886eb7c8c13bdcab77b04
Serylda <ericyzt1an@gmail.com>
2024-04-01 04:00:31 -0500

    itinerary page files

---
+++ itinAI-Project/ItineraryPageVC.swift
@@ -0,0 +1,25 @@
+//
+//  itineraryPageVC.swift
+//  itinAI-Project
+//
+//  Created by Eric Yang on 4/1/24.
+//
+
+import UIKit
+
+class ItineraryPageVC: UIViewController {
+
+    
+    @IBOutlet weak var bgImageView: UIImageView!
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var mainContentView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+}
